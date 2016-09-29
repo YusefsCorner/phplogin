7704aa78f664faf19fe4d3573179e8057e4f9f34
Yusef Ahmed <yusefa@mess.org>
2016-09-29 15:15:30 +0200

    creating the stylesheet and simple page and form styling
    I adding some styling to the login form interface, making it clearer
    better to use.

--- login.php
+++ login.php
@@ -1,12 +1,19 @@
 <!doctype html>
 <html>
 <head>
+<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 <meta charset="utf-8">
 <title>Login Below</title>
 </head>
 
 <body>
-<h1>Register</h1>
+<h1>Login</h1>
+
+<form action="login.php" method="post">
+        	<input type="text" name="email" placeholder="Enter your email"><br>
+        	<input type="password" name="password" placeholder="Enter password"><br>
+            <input type="submit">
+</form>
 
 </body>
 </html>
