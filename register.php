7b17e80cc2924deff9f1f642bad373b5838a64b3
Yusef Ahmed <yusefa@mess.org>
2016-09-29 16:24:14 +0200

    establishing connection between the database
    I established the connection to the database on the register.php page,
    using PDO since its the latest method.

--- register.php
+++ register.php
@@ -1,10 +1,22 @@
 <?php
 
+$server = 'localhost';
+$username = 'root';
+$password = 'root';
+$database = 'logins';
+
+try{
+	$conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
+} catch(PDOException $e){
+	die("Connection Failed: ". $e->getMessage());
+}	
+
+
 	if(!empty($_POST['email']) && !empty($_POST['password']));
 
 	//Enter the new user into the database
 
-	endif;	
+
 
 
 ?>
